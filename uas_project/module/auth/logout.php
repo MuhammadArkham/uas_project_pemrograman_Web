82b34ccd1f149dafe515c1f63011bc69e03dbdb2
MuhammadArkham <arkhamqwe123@gmail.com>
2026-01-07 08:47:53 +0700

    Delete uas_project directory

--- uas_project/module/auth/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_destroy();
-echo "<script>alert('Anda berhasil logout.'); window.location.href='index.php';</script>";
-?>
--- uas_project/module/barang/hapus.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id = $_GET['id'];
-$db = new Database();
-if($db->delete("barang", "WHERE id_barang='$id'")){
-    echo "<script>alert('Produk telah dihapus.'); window.location='?mod=barang';</script>";
-}
-?>
